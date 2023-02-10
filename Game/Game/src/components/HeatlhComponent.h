91538f3c41d5117ae5616b31f0a0aac6af3596fa
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2023-02-10 10:46:43 +0100

    Health controller

---
+++ Game/Game/src/components/HeatlhComponent.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Component.h"
+class HealthComponent : public Component {
+private:
+	int lifePoints;
+	int maxLife;
+	int modifiedMaxLife;
+public:
+	HealthComponent(Manager* mngr_, GameObject* gameObject, int Maxlife);
+	void receiveDamage(int damage);
+	void heal(int heal);
+	void die();
+	int getLife();
+	void setLife(double multiplier);
+	/*void timeDamage()*/
+};
