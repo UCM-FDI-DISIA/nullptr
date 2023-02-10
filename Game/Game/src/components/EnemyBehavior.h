fed5512c8c4ecb6099ed68511a99e00ca02d3309
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2023-02-10 12:51:40 +0100

    Update EnemyBehavior.h

--- Game/Game/src/components/EnemyBehavior.h
+++ Game/Game/src/components/EnemyBehavior.h
@@ -7,8 +7,6 @@ protected:
 	Transform* pos;
 	Transform* playerPos;
 	float speed;
-	float elapsedTime;
-	float actualTime;
 	// AttackComponent* attack (cuando este cerca del player, envia un mensaje para atacar)
 public:
 	EnemyBehavior(Manager* mgr, GameObject* gmObj,Transform* Pos, Transform* playerpos, float spd)
