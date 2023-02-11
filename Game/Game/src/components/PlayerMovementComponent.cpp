397d046529863e0d56ae5f48f716aa8d65494631
dlbcb <diegol08@ucm.es>
2023-02-11 18:16:27 +0100

    Aproximacion a diagonales y casos especiales

--- Game/Game/src/components/PlayerMovementComponent.cpp
+++ Game/Game/src/components/PlayerMovementComponent.cpp
@@ -39,5 +39,71 @@ void PlayerMovementComponent::handleInput() {
 	else if (InputHandler::instance()->isKeyUp(SDLK_s)) {
 		transform->setVel(Vector2D());
 	}
+	//Done
+	if (InputHandler::instance()->isKeyDown(SDLK_s)&& InputHandler::instance()->isKeyDown(SDLK_a)) {
+		//Movimiento abajo izquierda
+		transform->setVel(Vector2D(-playerSpeed, playerSpeed));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_s)) {
+		//Se levanta la s
+		transform->setVel(Vector2D(-playerSpeed, 0));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_a)) {
+		//Se levanta la a
+		transform->setVel(Vector2D(0, playerSpeed));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_s) && InputHandler::instance()->isKeyUp(SDLK_a)) {
+		//Se levantan ambas teclas
+		transform->setVel(Vector2D());
+	}
+	//Pendant
+	if (InputHandler::instance()->isKeyDown(SDLK_s)&& InputHandler::instance()->isKeyDown(SDLK_d)) {
+		//Movimiento abajo derecha
+		transform->setVel(Vector2D(playerSpeed, playerSpeed));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_s)) {
+		//Se levanta la s
+		transform->setVel(Vector2D(-playerSpeed, 0));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_a)) {
+		//Se levanta la a
+		transform->setVel(Vector2D(0, playerSpeed));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_s)&& InputHandler::instance()->isKeyUp(SDLK_d)) {
+		transform->setVel(Vector2D());
+	}
+	//Pendant
+	
+	if (InputHandler::instance()->isKeyDown(SDLK_w)&& InputHandler::instance()->isKeyDown(SDLK_d)) {
+		//Movimiento arriba derecha
+		transform->setVel(Vector2D(playerSpeed, -playerSpeed));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_s)) {
+		//Se levanta la s
+		transform->setVel(Vector2D(-playerSpeed, 0));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_a)) {
+		//Se levanta la a
+		transform->setVel(Vector2D(0, playerSpeed));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_w)&& InputHandler::instance()->isKeyUp(SDLK_d)) {
+		transform->setVel(Vector2D());
+	}
+	//Pendant
+	if (InputHandler::instance()->isKeyDown(SDLK_w)&& InputHandler::instance()->isKeyDown(SDLK_a)) {
+		//Movimiento arriba izquierda
+		transform->setVel(Vector2D(-playerSpeed, -playerSpeed));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_s)) {
+		//Se levanta la s
+		transform->setVel(Vector2D(-playerSpeed, 0));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_a)) {
+		//Se levanta la a
+		transform->setVel(Vector2D(0, playerSpeed));
+	}
+	else if (InputHandler::instance()->isKeyUp(SDLK_w)&& InputHandler::instance()->isKeyUp(SDLK_a)) {
+		transform->setVel(Vector2D());
+	}
 	
 }
