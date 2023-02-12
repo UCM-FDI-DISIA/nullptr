c2bce2489a4bf2e736c693a88fc941071f2219e7
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2023-02-12 18:54:42 +0100

    health

---
+++ Game/Game/src/components/HeatlhComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Component.h"
+class HealthComponent : public Component {
+private:
+	int lifePoints;
+	int maxLife;
+	int modifiedMaxLife;
+public:
+	static const int id = _HEALTH;
+	HealthComponent(Manager* mngr_, GameObject* gameObject, int Maxlife);
+	//Delete del padre
+	void receiveDamage(int damage);
+	void heal(int heal);
+	void die();
+	int getLife();
+	void setLife(double multiplier);
+	/*void timeDamage()*/
+};
