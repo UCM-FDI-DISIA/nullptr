f759ac464960661b0b867a547de349e93aa93a6e
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2023-02-12 20:01:11 +0100

    bullet update

--- Game/Game/src/gameObjects/Bullet.h
+++ Game/Game/src/gameObjects/Bullet.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "../core/GameObject.h"
+#include "../components/Transform.h"
 class Bullet:public GameObject
 {
 private:
 	int damage;
 public:
-	Bullet();
-	~Bullet();
+	// Se le pasa la posición de inicio de quien lo ha disparado y la dirección a la que va
+
+	Bullet(Vector2D pos, Vector2D dir, int damage)
+	{
+		addComponent<Transform>(pos, dir, 30, 30);
+		//Colisiones? CallBack? Necesita damage para bajar la vida
+	}
 };
