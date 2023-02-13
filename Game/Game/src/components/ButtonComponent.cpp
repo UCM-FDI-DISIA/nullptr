5c58bfd1a6cc999f739655292bfac5b3900d4cac
Rafaa0514 <99506556+Rafaa0514@users.noreply.github.com>
2023-02-13 15:56:54 +0100

    Botones funcionales

---
+++ Game/Game/src/components/ButtonComponent.cpp
@@ -0,0 +1,24 @@
+#include "ButtonComponent.h"
+#include "../core/GameObject.h"
+
+void ButtonComponent::update() {
+	int mouseX = 0, mouseY = 0;
+	SDL_GetMouseState(&mouseX, &mouseY);
+
+	if ((mouseX >= tr->getPos().getX() && mouseX < tr->getPos().getX() + tr->getWidth()) &&
+		(mouseY >= tr->getPos().getY() && mouseY < tr->getPos().getY() + tr->getHeight()))
+		state = OnOver;
+	else state = OnOut;
+
+}
+
+void ButtonComponent::handleInput() {
+	if (InputHandler::instance()->getMouseButtonState(InputHandler::LEFT) && state == OnOver) {
+		state = OnClick;
+		function(game);
+	}
+}
+
+void ButtonComponent::initComponent() {
+	tr = gObj->getComponent<Transform>();
+}
---
+++ Game/Game/src/managers/MainMenuManager.cpp
@@ -0,0 +1,14 @@
+#include "MainMenuManager.h"
+#include "../GameObjects/UI/Button.h"
+#include "../core/SDLApplication.h"
+
+MainMenuManager::MainMenuManager(SDLApplication* game) {
+	for (int i = 0; i < 3; i++) {
+		Button* button = new Button(Test, game, Vector2D(WIN_WIDTH / 2 - 79, (WIN_HEIGHT / 2 - 30) + i * 30));
+		addGameObject(button);
+	}
+}
+
+void MainMenuManager::Test(SDLApplication* game) {
+	cout << "HE SIDO CLICADO" << endl;
+}
