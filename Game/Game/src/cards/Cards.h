4a18f3395b1416bd3dbe2d0712fd00401453d0fb
dlbcb <diegol08@ucm.es>
2023-02-18 17:17:46 +0100

    Virtualización

--- Game/Game/src/cards/Cards.h
+++ Game/Game/src/cards/Cards.h
@@ -6,20 +6,20 @@ class GunCard : public Card {
 	public:
 		GunCard() : Card(10,6,35,0.5,0.005,"Pistola","Dispara de manera normal",
 			"Se disparan todas las balas como una escopeta",nullptr) {};
-		void attack(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
-		void ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
+		virtual void attack(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
+		virtual void ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
 };
 
 class SwordCard : public Card {
 	public:
 		SwordCard() : Card(20, 5, 80, 2, 0.5, "Espada", "", "", nullptr) {};
-		void attack(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
-		void ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
+		virtual void attack(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
+		virtual void ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
 };
 
 class LaserShadesCard : public Card {
 	public:
 		LaserShadesCard() : Card(20, 5, 80, 2, 0.5, "Gafas Láser", "", "", nullptr) {};
-		void attack(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
-		void ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
+		virtual void attack(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
+		virtual void ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
 };
--- Game/Game/src/core/Card.h
+++ Game/Game/src/core/Card.h
@@ -23,8 +23,8 @@ class Card
 			damage(_damage),maxUses(_maxUses),remainingUses(_maxUses),mana(_mana),downtime(_downtime),bulletSpeed(_bulletSpeed),
 			name(_name), attackText(_attackText), abilityText(_abilityText), texture(_texture) {};
 		static Card getCard(CardId type);
-		virtual void attack(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
-		virtual void ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where);
+		virtual void attack(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where) {}
+		virtual void ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where) {}
 		int getUses() { return remainingUses; }
 		void use() { remainingUses -= 1;}
 		void resetUses() { remainingUses = maxUses;}
