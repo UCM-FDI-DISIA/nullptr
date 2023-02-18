3494c9a7592eed79a605aae87c24ebef35e5fce3
Javier Murillo González <javmuril@ucm.es>
2023-02-18 21:23:37 +0100

    melee refactorizado

--- Game/Game/src/gameObjects/MeleeEnemy.cpp
+++ Game/Game/src/gameObjects/MeleeEnemy.cpp
@@ -1,12 +1,12 @@
 #include "MeleeEnemy.h"
 #include "../core/SDLApplication.h"
 
-MeleeEnemy::MeleeEnemy(SDLApplication* game, Vector2D pos, Vector2D vel, int life, Player* player, Transform* camera)
-{
-	addComponent<Transform>(pos, vel, 30, 60, 0);
-	addComponent<Image>(game->getTexture("Enemy"), camera);
+MeleeEnemy::MeleeEnemy() {}
+
+void MeleeEnemy::initGameObject(SDLApplication* game, Vector2D pos, int life, Player* player) {
+	addComponent<Transform>(pos, Vector2D(0,0), 30, 60, 0);
+	addComponent<Image>(game->getTexture("Enemy"));
 	addComponent<MeleeBehaviour>(1000, float(0.001), 3, 30, 4, player);
 	//addComponent<ColliderComponent>();
 	//addComponent<HealthComponent>(life);
-
 }
