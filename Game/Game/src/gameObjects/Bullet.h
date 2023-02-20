d60afbd68fdbd11fa559e8ff9eebbc065f34ade1
Rafaa0514 <99506556+Rafaa0514@users.noreply.github.com>
2023-02-20 11:57:14 +0100

    Nodos de distinto color

--- Game/Game/src/gameObjects/Bullet.h
+++ Game/Game/src/gameObjects/Bullet.h
@@ -12,21 +12,6 @@
 class Bullet:public GameObject
 {
 public:
-	// Se le pasa la posicion de inicio de quien lo ha disparado y la direccion a la que va
-	/*Bullet(Vector2D pos, Vector2D dir,Camera* camera, int dmg, vector<GameObject*> target, SDLApplication* game)
-	{
-		addComponent<BulletBehavior>(dmg, target);
-		addComponent<Transform>(pos, dir, 30, 30);
-		addComponent<Image>(SDLApplication::getTexture("Bullet"), camera->getComponent<Transform>());
-		addComponent<ColliderComponent>();
-	}
-	Bullet(Vector2D pos, Vector2D dir,Camera* camera, int dmg, GameObject* target, SDLApplication* game)
-	{
-		addComponent<BulletBehavior>(dmg, target);
-		addComponent<Transform>(pos, dir, 30, 30);
-		addComponent<Image>(SDLApplication::getTexture("Bullet"), camera->getComponent<Transform>());
-		addComponent<ColliderComponent>();
-	}*/
 
 	virtual void initGameObject(Vector2D pos, Vector2D dir, int dmg, vector<GameObject*>* target) {
 		addComponent<BulletBehavior>(dmg, target);
