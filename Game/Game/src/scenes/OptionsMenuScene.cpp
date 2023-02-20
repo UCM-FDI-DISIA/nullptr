2bede760b888c43384375093bf804bc5e1a0b7a0
Pavipollo17 <99996169+PabloGonzalezArroyoo@users.noreply.github.com>
2023-02-20 11:12:19 +0100

    Pantalla semiredimensionable

--- Game/Game/src/scenes/OptionsMenuScene.cpp
+++ Game/Game/src/scenes/OptionsMenuScene.cpp
@@ -6,8 +6,15 @@ OptionsMenuScene::OptionsMenuScene() : GameState() {
 
 	addGameObject<Button>(mainMenu, SDLApplication::instance(), Vector2D(WIN_WIDTH / 2 - 79, (WIN_HEIGHT / 4) + 50),
 		EXIT, BUTTON_SPRITE_WIDTH, BUTTON_SPRITE_HEIGHT, BUTTON_SPRITE_ROWS, BUTTON_SPRITE_COLUMS, BUTTON_WIDTH, BUTTON_HEIGHT);
+
+	addGameObject<Button>(toggleScreen, SDLApplication::instance(), Vector2D(WIN_WIDTH / 2 - 79, (WIN_HEIGHT / 4) + 200),
+		OPTIONS, BUTTON_SPRITE_WIDTH, BUTTON_SPRITE_HEIGHT, BUTTON_SPRITE_ROWS, BUTTON_SPRITE_COLUMS, BUTTON_WIDTH, BUTTON_HEIGHT);
 }
 
 void OptionsMenuScene::mainMenu() {
 	SDLApplication::newScene<MainMenuScene>();
+}
+
+void OptionsMenuScene::toggleScreen() {
+	SDLUtils::instance()->toggleFullScreen();
 }
