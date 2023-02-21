cf122eda5de26cc03cdf0647020c87f23e986aad
MiguelCGa <micurros@ucm.es>
2023-02-21 12:49:08 +0100

    Solución de conflictos 2

--- Game/Game/src/scenes/GameOverScene.cpp
+++ Game/Game/src/scenes/GameOverScene.cpp
@@ -21,7 +21,7 @@ GameOverScene::GameOverScene() {
 	An->attachToCamera();
 
 	auto but = addGameObject<Button>(mainMenu, SDLApplication::instance(), Vector2D(WIN_WIDTH / 2 - 79, WIN_HEIGHT * 2 / 3),
-		EXIT, BUTTON_SPRITE_WIDTH, BUTTON_SPRITE_HEIGHT, BUTTON_SPRITE_ROWS, BUTTON_SPRITE_COLUMS,BUTTON_WIDTH,BUTTON_HEIGHT, marco);
+		EXIT, MAINMENU_BUTTON_WIDTH, MAINMENU_BUTTON_HEIGHT, BUTTON_SPRITE_WIDTH, BUTTON_SPRITE_HEIGHT, BUTTON_SPRITE_ROWS, BUTTON_SPRITE_COLUMS, marco);
 	but->getComponent<Animator>()->attachToCamera();
 }
 
