982a12585418ff0c208e3e3ae89127bb1d17a335
Rafaa0514 <99506556+Rafaa0514@users.noreply.github.com>
2023-02-26 16:24:46 +0100

    Otra constructora en el AnimatorInfo

--- Game/Game/src/gameObjects/UI/Button.h
+++ Game/Game/src/gameObjects/UI/Button.h
@@ -14,13 +14,14 @@ struct AnimatorInfo {
 
 	AnimatorInfo(string _k) : key(_k), w(MM_BUTTON_WIDTH), h(MM_BUTTON_HEIGHT), fw(BUTTON_SPRITE_WIDTH), fh(BUTTON_SPRITE_HEIGHT), 
 		rows(BUTTON_SPRITE_ROWS), cols(BUTTON_SPRITE_COLUMS) { }
+	AnimatorInfo (string _k, int _w, int _h, int _fw, int _fh, int _r, int _c) : 
+		key(_k), w(_w), h(_h), fw(_fw), fh(_fh), rows(_r), cols(_c) { }
 };
 
 class Button : public GameObject {
 private:
 public:
 	// Añade los componentes al botón y crea sus animaciones
-	// // cleon says:  11 parámetros == 11 gatitos que sufren
 	virtual void initGameObject(CallBack _cb, SDLApplication* game, Vector2D _pos, AnimatorInfo _animInfo, GameObject* _frame = nullptr);
 
 	// Crear animaciones
