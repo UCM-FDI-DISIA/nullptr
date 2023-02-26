4113a9039c69012296aab3bcd07949d58bc40501
dlbcb <diegol08@ucm.es>
2023-02-26 16:28:37 +0100

    Comentario chiquito, para clarificar

--- Game/Game/src/gameObjects/UI/Button.h
+++ Game/Game/src/gameObjects/UI/Button.h
@@ -11,9 +11,10 @@ struct AnimatorInfo {
 	int w, h;
 	int fw, fh;
 	int rows, cols;
-
+	//Con constantes para botones genéricos
 	AnimatorInfo(string _k) : key(_k), w(MM_BUTTON_WIDTH), h(MM_BUTTON_HEIGHT), fw(BUTTON_SPRITE_WIDTH), fh(BUTTON_SPRITE_HEIGHT), 
 		rows(BUTTON_SPRITE_ROWS), cols(BUTTON_SPRITE_COLUMS) { }
+	//Con parámetros específicos
 	AnimatorInfo (string _k, int _w, int _h, int _fw, int _fh, int _r, int _c) : 
 		key(_k), w(_w), h(_h), fw(_fw), fh(_fh), rows(_r), cols(_c) { }
 };
