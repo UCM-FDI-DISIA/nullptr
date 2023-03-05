b47e653b2fde57853bd1469db06613f69ab9dd39
josepk01 <72452701+josepk01@users.noreply.github.com>
2023-03-05 16:01:42 +0100

    Update MeleeEnemy.cpp

--- Game/Game/src/gameObjects/MeleeEnemy.cpp
+++ Game/Game/src/gameObjects/MeleeEnemy.cpp
@@ -2,12 +2,12 @@
 #include "../core/SDLApplication.h"
 
 void MeleeEnemy::initGameObject(Vector2D pos, int life, Player* player) {
-	addComponent<Transform>(pos, Vector2D(0,0), MELE_ENEMY_INITIAL_WIDTH, MELE_ENEMY_INITIAL_HEIGHT, 0);
+	addComponent<Transform>(pos, Vector2D(0,0), MELEE_ENEMY_INITIAL_WIDTH, MELEE_ENEMY_INITIAL_HEIGHT, 0);
 	//addComponent<Image>(SDLApplication::getTexture("Enemy"));
 	addComponent<MeleeBehaviour>(1000, float(0.001), 3, 30, 1000, player);
 	addComponent<ColliderComponent>();
 	addComponent<HealthComponent>(life);
-	addComponent<EnemyAnimator>(SDLApplication::getTexture(MELE_ENEMY), MELE_ENEMY_SPRITE_WIDTH, MELE_ENEMY_SPRITE_HEIGHT,
-		MELE_ENEMY_SPRITE_ROWS, MELE_ENEMY_SPRITE_COLS);
+	addComponent<EnemyAnimator>(SDLApplication::getTexture(MELE_ENEMY), MELEE_ENEMY_SPRITE_WIDTH, MELEE_ENEMY_SPRITE_HEIGHT,
+		MELEE_ENEMY_SPRITE_ROWS, MELEE_ENEMY_SPRITE_COLS);
 
 }
