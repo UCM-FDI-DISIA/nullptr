ead5ee92830a180f32e5707cb3e258fa7771af5b
Pavipollo17 <99996169+PabloGonzalezArroyoo@users.noreply.github.com>
2023-03-08 16:55:20 +0100

    Números en interfaz terminados
    Se renderizan y cambian según la vida

--- Game/Game/src/gameObjects/UI/StatisticsUI.cpp
+++ Game/Game/src/gameObjects/UI/StatisticsUI.cpp
@@ -21,64 +21,66 @@ void StatisticsUI::initGameObject(int life, int mana) {
 	fullLife = life;
 	fullMana = mana;
 
-	int cents = fullLife / 100;
-	int dec = (fullLife - cents * 100) / 10;
-	int unid = (fullLife - cents * 100 - dec * 10);
-
 	// Crear los numeros de vida
 	for (int i = 0; i < 7; i++) {
+		// Crear el objeto
 		GameObject* number = new GameObject();
-		number->addComponent<Transform>(Vector2D(WIN_WIDTH / 2 + 60 + (12 * i), 46), Vector2D(), 9, 18, 0);
 		
-		auto anim = number->addComponent<Animator>(SDLApplication::getTexture("statisticsNumbers"), 9, 18, 6, 2);
-		for (int j = 0; j < 11; j++) anim->createAnim(to_string(j), j, j, 1, 0);
-		if (i == 0 || i == 4) anim->play(to_string(cents));
-		else if (i == 1 || i == 5) anim->play(to_string(dec));
-		else if (i == 2 || i == 6) anim->play(to_string(unid));
-		else if (i == 3) anim->play(to_string(10));
+		// Añadir componentes y crea animaciones
+		number->addComponent<Transform>(Vector2D(WIN_WIDTH / 2 + 60 + (12 * i), 46), Vector2D(), 9, 18, 0);
+		createNumberAnims(number, fullLife, i);
 
+		// Añadir a su vector
 		lifeCounter.push_back(number);
 	}
 
-	cents = fullMana / 100;
-	dec = (fullMana - cents * 100) / 10;
-	unid = (fullMana - cents * 100 - dec * 10);
-
 	// Crear los numeros de maná
 	for (int i = 0; i < 7; i++) {
+		// Crear el objeto
 		GameObject* number = new GameObject();
+
+		// Añadir componentes y crea animaciones
 		number->addComponent<Transform>(Vector2D(WIN_WIDTH / 2 - 140 + (12 * i), 46), Vector2D(), 9, 18, 0);
+		createNumberAnims(number, fullMana, i);
 
-		auto anim = number->addComponent<Animator>(SDLApplication::getTexture("statisticsNumbers"), 9, 18, 6, 2);
-		for (int j = 0; j < 11; j++) anim->createAnim(to_string(j), j, j, 1, 0);
-		if (i == 0 || i == 4) anim->play(to_string(cents));
-		else if (i == 1 || i == 5) anim->play(to_string(dec));
-		else if (i == 2 || i == 6) anim->play(to_string(unid));
-		else if (i == 3) anim->play(to_string(10));
-
+		// Añadir a su vector
 		manaCounter.push_back(number);
 	}
 }
 
 // Crea la barra de vida
 void StatisticsUI::createLifeBar(int value) {
+	// Añadir el objeto
 	healthBar = new GameObject();
+
+	// Componentes transform y animator
 	healthBar->addComponent<Transform>(LIFEBAR_POSITION, Vector2D(), BAR_WIDTH * 2.5, BAR_HEIGHT * 2.5);
 	auto anim = healthBar->addComponent<Animator>(SDLApplication::getTexture(LIFEBAR), BAR_WIDTH, BAR_HEIGHT, 4, 3);
+	
+	// Mantener en la cámara y crear y reproducir animaciones
 	anim->attachToCamera();
 	anim->createAnim(LIFEBAR, 0, 10, 5, -1);
 	anim->play(LIFEBAR);
+
+	// Componente de barra
 	healthBar->addComponent<BarComponent>(true, value);
 }
 
 // Crea la barra de mana
 void StatisticsUI::createManaBar(int value) {
+	// Añadir el objeto
 	manaBar = new GameObject();
+
+	// Componentes transform y animator
 	manaBar->addComponent<Transform>(MANABAR_POSITION, Vector2D(), BAR_WIDTH * 2.5, BAR_HEIGHT * 2.5);
 	auto anim = manaBar->addComponent<Animator>(SDLApplication::getTexture(MANABAR), BAR_WIDTH, BAR_HEIGHT, 4, 3);
+	
+	// Mantener en la cámara y crear y reproducir animaciones
 	anim->attachToCamera();
 	anim->createAnim(MANABAR, 0, 10, 5, -1);
 	anim->play(MANABAR);
+
+	// Componente de barra
 	manaBar->addComponent<BarComponent>(false, value);
 }
 
@@ -100,14 +102,59 @@ void StatisticsUI::render() const {
 	for (int i = 0; i < 7; i++) manaCounter[i]->render();
 }
 
+// Al cambiar la vida, actualizar componentes
 void StatisticsUI::OnHealthChanges(float value) {
-	float newSrcRectFactor = (100 * value) / (fullLife * 100);
-	healthBar->getComponent<Animator>()->setSrcRectRelativeWidth(newSrcRectFactor);
-	// healthBar->getComponent<BarComponent>()->changeBar(value);
+	changeAnimatorSrcRelativeWidth(healthBar, fullLife, value);
+	changeNumbers(lifeCounter, value);
 }
 
+// Al cambiar el maná, actualizar componentes
 void StatisticsUI::OnManaChanges(float value) {
-	float newSrcRectFactor = (100 * value) / (fullMana * 100);
-	manaBar->getComponent<Animator>()->setSrcRectRelativeWidth(newSrcRectFactor);
+	// Cambiar el tamaño de la animación respecto al maná gastado
+	changeAnimatorSrcRelativeWidth(manaBar, fullMana, value);
+
 	manaBar->getComponent<BarComponent>()->changeBar(value);
+	changeNumbers(manaCounter, value);
+}
+
+// Crear los números de la interfaz
+void StatisticsUI::createNumberAnims(GameObject* obj, int value, int i) {
+	// Obtener unidades del valor entrante
+	tuple<cents, decs, unids> units = getUnits(value);
+
+	// Añadir animator y crear animaciones
+	auto anim = obj->addComponent<Animator>(SDLApplication::getTexture("statisticsNumbers"), 9, 18, 6, 2);
+	for (int j = 0; j < 11; j++) anim->createAnim(to_string(j), j, j, 1, 0);
+	
+	// Reproducir "animacion" con el número correpondien
+	if (i == 0 || i == 4) anim->play(to_string(get<0>(units)));
+	else if (i == 1 || i == 5) anim->play(to_string(get<1>(units)));
+	else if (i == 2 || i == 6) anim->play(to_string(get<2>(units)));
+	else if (i == 3) anim->play(to_string(10));
+}
+
+// Cambia el valor de los números activan su animación correspondiente
+void StatisticsUI::changeNumbers(vector<GameObject*> vect, int value) {
+	// Obtener unidades
+	tuple<cents, decs, unids> units = getUnits(value);
+	
+	// 0 centenas - 1 decenas - 2 unidades
+	vect[0]->getComponent<Animator>()->play(to_string(get<0>(units)));
+	vect[1]->getComponent<Animator>()->play(to_string(get<1>(units)));
+	vect[2]->getComponent<Animator>()->play(to_string(get<2>(units)));
+}
+
+// Devuelve una tupla con el valor de las centenas, las decenas y las unidades
+tuple<cents, decs, unids> StatisticsUI::getUnits(int value) {
+	int cents = value / 100;
+	int decs = (value - cents * 100) / 10;
+	int unids = (value - cents * 100 - decs * 10);
+
+	return make_tuple(cents, decs, unids);
+}
+
+// Cálcula y cambia el ancho de animación al correspondiente
+void StatisticsUI::changeAnimatorSrcRelativeWidth(GameObject* bar, float maxValue, float value) {
+	float newSrcRectFactor = (100 * value) / (maxValue * 100);
+	bar->getComponent<Animator>()->setSrcRectRelativeWidth(newSrcRectFactor);
 }
--- Game/Game/src/gameObjects/UI/StatisticsUI.h
+++ Game/Game/src/gameObjects/UI/StatisticsUI.h
@@ -2,6 +2,12 @@
 #include "../../core/SDLApplication.h"
 #include "../GameObject.h"
 #include "../../components/Player components/BarComponent.h"
+#include <tuple>
+
+// Tipos propios
+using decs = int;
+using cents = int;
+using unids = int;
 
 class StatisticsUI : public GameObject {
 private:
@@ -22,15 +28,26 @@ private:
 	float fullLife;
 
 public:
+	// Inicializar objeto
 	void initGameObject(int life, int mana);
 
 	// Métodos virtuales
 	virtual void render() const;
 	virtual void update();
 
+	// Números de maná y vida
+	void createNumberAnims(GameObject* obj, int value, int i);
+
+	// Crear las barras de mana y de vida
 	void createLifeBar(int value);
 	void createManaBar(int value);
 
+	// Métodos para cambios en las barras anteriores
 	void OnManaChanges(float value);
 	void OnHealthChanges(float value);
+	void changeNumbers(vector<GameObject*> vect, int value);
+
+	// Cálculos
+	tuple<cents, decs, unids> getUnits(int value);
+	void changeAnimatorSrcRelativeWidth(GameObject* bar, float maxValue, float value);
 };
