b4ac527e0d503a45027f1b1fc3608ef29414df68
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2023-03-09 13:01:55 +0100

    1er commit, spritesheet montado

--- Game/Game/src/components/Player components/PlayerAnimator.cpp
+++ Game/Game/src/components/Player components/PlayerAnimator.cpp
@@ -4,4 +4,5 @@
 PlayerAnimator::PlayerAnimator(Texture* _texture, int _w, int _h, int _r, int _c) :
 	CharacterAnimator(_texture, _w, _h, _r, _c,
 		Animation(PLAYER_IDLE_INITIAL_FRAME, PLAYER_IDLE_FINAL_FRAME, PLAYER_IDLE_FRAME_RATE, -1),
-		Animation(PLAYER_MOVE_INITIAL_FRAME, PLAYER_MOVE_FINAL_FRAME, PLAYER_MOVE_FRAME_RATE, -1)) {}
+		Animation(PLAYER_MOVE_INITIAL_FRAME, PLAYER_MOVE_FINAL_FRAME, PLAYER_MOVE_FRAME_RATE, -1)
+	) {}
