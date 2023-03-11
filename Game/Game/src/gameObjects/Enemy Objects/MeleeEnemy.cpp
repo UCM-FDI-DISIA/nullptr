03ebc5a7085f911f6d27f1cf7544805ae44ccba7
josepk01 <72452701+josepk01@users.noreply.github.com>
2023-03-11 20:17:00 +0100

    Update MeleeEnemy.cpp

--- Game/Game/src/gameObjects/Enemy Objects/MeleeEnemy.cpp
+++ Game/Game/src/gameObjects/Enemy Objects/MeleeEnemy.cpp
@@ -9,7 +9,7 @@ void MeleeEnemy::initGameObject(Vector2D pos, int life, Player* player) {
 	*/
 	addComponent<Transform>(pos, Vector2D(0, 0), MELEE_ENEMY_WIDTH * 1.5, MELEE_ENEMY_HEIGHT * 1.5, 0);
 	addComponent<ColliderComponent>(_grp_PLAYER);
-	addComponent<MeleeBehaviour>(1000, ENEMY_SPEED, 0, MELEE_ENEMY_WIDTH, 1000, player);
+	addComponent<MeleeBehaviour>(1000, ENEMY_SPEED, 50, MELEE_ENEMY_WIDTH, 1000, player);
 	addComponent<HealthComponent>(life);
 	addComponent<MeleeEnemyAnimator>();
 }
