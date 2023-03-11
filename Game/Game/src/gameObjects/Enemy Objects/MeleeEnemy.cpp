28e9458221f36d9cd17cd88f574ce00cfbcafa16
josepk01 <72452701+josepk01@users.noreply.github.com>
2023-03-11 19:38:50 +0100

    errores en animacion por refactorizacion

--- Game/Game/src/gameObjects/Enemy Objects/MeleeEnemy.cpp
+++ Game/Game/src/gameObjects/Enemy Objects/MeleeEnemy.cpp
@@ -4,9 +4,12 @@
 #include "../../scenes/BattleScene.h"
 
 void MeleeEnemy::initGameObject(Vector2D pos, int life, Player* player) {
-	addComponent<Transform>(pos, Vector2D(0,0), MELEE_ENEMY_WIDTH *1.5, MELEE_ENEMY_HEIGHT * 1.5, 0);
-	addComponent<MeleeBehaviour>(1000, ENEMY_SPEED, 0, MELEE_ENEMY_WIDTH , 1000, player);
+/*
 	addComponent<ColliderComponent>(getComponent<MeleeBehaviour>()->meleeAttack(),_grp_PLAYER);
+	*/
+	addComponent<Transform>(pos, Vector2D(0, 0), MELEE_ENEMY_WIDTH * 1.5, MELEE_ENEMY_HEIGHT * 1.5, 0);
+	addComponent<ColliderComponent>(_grp_PLAYER);
+	addComponent<MeleeBehaviour>(1000, ENEMY_SPEED, 0, MELEE_ENEMY_WIDTH, 1000, player);
 	addComponent<HealthComponent>(life);
 	addComponent<MeleeEnemyAnimator>();
 }
