4e252b12450c241cb8ccd579fc1eb0de6f7e9a34
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2023-03-17 22:17:25 +0100

    Correcciones de codigo

--- Game/Game/src/components/Enemy components/EterBehaviour.cpp
+++ Game/Game/src/components/Enemy components/EterBehaviour.cpp
@@ -1,7 +1,6 @@
 #include "EterBehaviour.h"
 
-EterBehaviour::EterBehaviour(float groundTime, Transform* player)
-{
+EterBehaviour::EterBehaviour(float groundTime, Transform* player) {
 	floorTime = groundTime;
 	elapsedTime = 0;
 	speeding = 0;
@@ -9,22 +8,18 @@ EterBehaviour::EterBehaviour(float groundTime, Transform* player)
 	dest = Vector2D(WIN_WIDTH / 2, 0);
 	dir = Vector2D(0, 1);
 }
-void EterBehaviour::initComponent()
-{
+void EterBehaviour::initComponent() {
 	myTransform = gObj->getComponent<Transform>();
 }
-void EterBehaviour::update()
-{
+void EterBehaviour::update() {
 	dest = Vector2D(plyr->getPos().getX(), plyr->getPos().getY() - WIN_HEIGHT / 2);
 	elapsedTime += SDLApplication::instance()->getDeltaTime();
-	if (elapsedTime >= floorTime)
-	{
+	if (elapsedTime >= floorTime) {
 		dir = dir.rotate(myTransform->getVel().angle(dest - myTransform->getPos()) > 0 ? 0.3f : -0.3f);
 		speeding += 0.5;
 		myTransform->setVel(dir*speeding);
 	}
-	if ((myTransform->getPos().getX() <= dest.getX()+ 30 && myTransform->getPos().getX() >= dest.getX() - 30) && myTransform->getPos().getY() <= dest.getY())
-	{
+	if ((myTransform->getPos().getX() <= dest.getX()+ 30 && myTransform->getPos().getX() >= dest.getX() - 30) && myTransform->getPos().getY() <= dest.getY()) {
 		gObj->setAlive(false);
 		dynamic_cast<BattleScene*>(gStt)->onEtherChanges(ETHER_VALUE);
 	}
