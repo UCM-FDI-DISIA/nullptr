e425bd8681d4f5c42f5467309865884c39d8d47b
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2023-04-20 13:24:17 +0200

    Anim SwordSpin

--- Game/Game/src/gameObjects/Card Objects/Sword.cpp
+++ Game/Game/src/gameObjects/Card Objects/Sword.cpp
@@ -15,9 +15,17 @@ void SwordCard::attack(Vector2D playerPos, Vector2D mousePos, float attackMult, 
 // Crea un gObj Spin en el centro del jugador
 void SwordCard::ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where) {
 	
-	Hitbox::HitboxData data = {playerPos, VECTOR_ZERO, 0, 300, 300, SWORD_SPIN, _grp_ENEMIES};
+	Hitbox::HitboxData  data = { playerPos, VECTOR_ZERO, 0, 300, 300, "null", _grp_ENEMIES };
 
 	where->addGameObject<Hitbox>(_grp_PLYR_ATTACK, damage * attackMult, false, false, 0.1, data);
+
+	auto slashAnim = where->addGameObject<GameObject>();
+	slashAnim->addComponent<Transform>(playerPos - Vector2D(150, 150), VECTOR_ZERO, 300, 300);
+	auto anim = slashAnim->addComponent<Animator>(SDLApplication::getTexture("NewSwordSpin"), 192, 192, 1, 8);
+	anim->createAnim("SwordSpin", Animation(0, 7, 10, 1));
+	anim->play("SwordSpin");
+	slashAnim->addComponent<LifeTimeComponent>(1);
+
 
 	remainingUses = 0;
 }
