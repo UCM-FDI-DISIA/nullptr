2efacbd58e84557c52f2207684dedb5a4640ff57
MiguelCGa <micurros@ucm.es>
2023-04-26 17:21:38 +0200

    Borrado comentario MovComp

--- Game/Game/src/components/Player components/PlayerMovementComponent.cpp
+++ Game/Game/src/components/Player components/PlayerMovementComponent.cpp
@@ -22,28 +22,6 @@ void PlayerMovementComponent::setDirection(Vector2D dir) {
 	// Normalizar y cambiar módulo
 	transform->setVel(dir.normalize() * playerSpeed);
 }
-void PlayerMovementComponent::handleInput() {
-	//// Dirección
-	//Vector2D vel(0, 0);
-	//vel = vel + Vector2D(gmCtrl_.movementX(), gmCtrl_.movementY());
-
-	//// Límites
-	//Vector2D pos = transform->getPos();
-	//if (pos.getX() < PLAYER_INITIAL_WIDTH) transform->setX(PLAYER_INITIAL_WIDTH);
-	//else if (pos.getX() > FLOOR_WIDTH - PLAYER_INITIAL_WIDTH) transform->setX(FLOOR_WIDTH - PLAYER_INITIAL_WIDTH);
-
-	//if (pos.getY() < 0 - PLAYER_INITIAL_HEIGHT / 3) transform->setY(0 - PLAYER_INITIAL_HEIGHT / 3);
-	//else if (pos.getY() > FLOOR_HEIGHT - PLAYER_INITIAL_HEIGHT) transform->setY(FLOOR_HEIGHT - PLAYER_INITIAL_HEIGHT);
-
-	//// Normalizar y cambiar módulo 
-	//vel = vel.normalize() * playerSpeed;
-	//transform->setVel(vel);
-
-	//// Activar la pausa lanzando su estado
-	//if (gmCtrl_.pause()) {
-	//	SDLApplication::pauseGame();
-	//}
-}
 
 void PlayerMovementComponent::setPlayerSpeed(float newSpeed) {
 	playerSpeed = newSpeed;
