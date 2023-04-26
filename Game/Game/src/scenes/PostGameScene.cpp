5bd708290c47f12267355091afd3629028f3a9af
Alejandro González <90796690+algonz39@users.noreply.github.com>
2023-04-26 12:46:18 +0200

    Ajustes texto

--- Game/Game/src/scenes/PostGameScene.cpp
+++ Game/Game/src/scenes/PostGameScene.cpp
@@ -19,43 +19,43 @@ PostGameScene::PostGameScene(int dmg, int mlee, int rngd, int tnk, int mny, floa
 	Frame->addComponent<Image>(SDLApplication::getTexture("PostGameFrame"));
 
 	GameObject* title = addGameObject(_grp_UI);
-	title->addComponent<Transform>(Vector2D(60, 35), VECTOR_ZERO, 400, 100);
+	title->addComponent<Transform>(Vector2D(70, 65), VECTOR_ZERO, 400, 100);
 	title->addComponent<TextComponent>(&sdlutils().fonts().at("ARIAL48"), "ESTADISTICAS");
 
 	GameObject* enemiesKilled = addGameObject(_grp_UI);
-	enemiesKilled->addComponent<Transform>(Vector2D(60, 125), VECTOR_ZERO, 400, 100);
+	enemiesKilled->addComponent<Transform>(Vector2D(70, 155), VECTOR_ZERO, 400, 100);
 	enemiesKilled->addComponent<TextComponent>(&sdlutils().fonts().at("ARIAL36"), "Enemigos derrotados: " + to_string(mlee + rngd + tnk));
 	if (mlee > 0) {
 		i++;
 		GameObject* melee = addGameObject(_grp_UI);
-		melee->addComponent<Transform>(Vector2D(80, 125 + 60 * i), VECTOR_ZERO, 400, 100);
+		melee->addComponent<Transform>(Vector2D(90, 155 + 60 * i), VECTOR_ZERO, 400, 100);
 		melee->addComponent<TextComponent>(&sdlutils().fonts().at("ARIAL36"), "Melee: " + to_string(mlee));
 	}
 	if (rngd > 0) {
 		i++;
 		GameObject* ranged = addGameObject(_grp_UI);
-		ranged->addComponent<Transform>(Vector2D(80, 125 + 60 * i), VECTOR_ZERO, 400, 100);
+		ranged->addComponent<Transform>(Vector2D(90, 155 + 60 * i), VECTOR_ZERO, 400, 100);
 		ranged->addComponent<TextComponent>(&sdlutils().fonts().at("ARIAL36"), "Ranged: " + to_string(rngd));
 	}
 	if (tnk > 0) {
 		i++;
 		GameObject* tank = addGameObject(_grp_UI);
-		tank->addComponent<Transform>(Vector2D(80, 125 + 60 * i), VECTOR_ZERO, 400, 100);
+		tank->addComponent<Transform>(Vector2D(90, 155 + 60 * i), VECTOR_ZERO, 400, 100);
 		tank->addComponent<TextComponent>(&sdlutils().fonts().at("ARIAL36"), "Tank: " + to_string(tnk));
 	}
 	GameObject* damage = addGameObject(_grp_UI);
-	damage->addComponent<Transform>(Vector2D(60, 215 + 60 * i), VECTOR_ZERO, 400, 100);
+	damage->addComponent<Transform>(Vector2D(70, 245 + 60 * i), VECTOR_ZERO, 400, 100);
 	damage->addComponent<TextComponent>(&sdlutils().fonts().at("ARIAL36"), "Vida perdida: " + to_string(dmg));
 
 	GameObject* time = addGameObject(_grp_UI);
-	time->addComponent<Transform>(Vector2D(60, 305 + 60 * i), VECTOR_ZERO, 400, 100);
+	time->addComponent<Transform>(Vector2D(70, 335 + 60 * i), VECTOR_ZERO, 400, 100);
 	std::stringstream ss;
 	ss << std::fixed << std::setprecision(2) << tm;
 	std::string mystring = ss.str();
 	time->addComponent<TextComponent>(&sdlutils().fonts().at("ARIAL36"), "Tiempo empleado: " + mystring + " s");
 
 	GameObject* money = addGameObject(_grp_UI);
-	money->addComponent<Transform>(Vector2D(60, 395 + 180), VECTOR_ZERO, 400, 100);
+	money->addComponent<Transform>(Vector2D(70, 425 + 180), VECTOR_ZERO, 400, 100);
 	money->addComponent<TextComponent>(&sdlutils().fonts().at("ARIAL36"), "Dinero obtenido: " + to_string(mny));
 
 
