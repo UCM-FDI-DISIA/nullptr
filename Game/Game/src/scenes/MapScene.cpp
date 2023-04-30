1c6509ec8296a050d87fb1d28a7343a484b84333
MiguelCGa <micurros@ucm.es>
2023-04-30 12:28:58 +0200

    botón cofre

--- Game/Game/src/scenes/MapScene.cpp
+++ Game/Game/src/scenes/MapScene.cpp
@@ -57,6 +57,15 @@ MapScene::MapScene() {
 
 	// Botón salir
 	exitButton = createButton(MS_EXIT_BUTTON_POS, MS_EXITFRAME_BUTTON_POS, []() { SDLApplication::newScene<MainMenuScene>(); }, EXIT);
+	
+	
+	auto animator = addGameObject<Button>([n = nodeMap[0][1]]() { gameMap().setCurrentNode(n); SDLApplication::pushNewScene<ChestScene>(); }, MM_EXIT_BUTTON_POS, AnimatorInfo(CHEST_NODE_TEXTURE_KEY, NODE_WIDTH, NODE_HEIGHT, NODE_FRAME_WIDTH, NODE_FRAME_HEIGHT, NODE_FRAME_ROWS, NODE_FRAME_COLUMNS))->getComponent<Animator>();
+	animator->createAnim(ONOUT, NODE_BUTTON_ONOUT_START_FRAME, NODE_BUTTON_ONOUT_END_FRAME, ONCLICK_ONOUT_SPEED, -1);
+	animator->createAnim(ONOVER, NODE_BUTTON_ONOVER_START_FRAME, NODE_BUTTON_ONOVER_END_FRAME, ONOVER_SPEED, 1);
+	animator->createAnim(ONCLICK, NODE_BUTTON_ONCLICK_START_FRAME, NODE_BUTTON_ONCLICK_END_FRAME, ONCLICK_ONOUT_SPEED, -1);
+	animator->createAnim(ONLOCKED, NODE_BUTTON_ONLOCKED_START_FRAME, NODE_BUTTON_ONLOCKED_END_FRAME, LOGO_FRAME_RATE, -1);
+	animator->createAnim(ONCOMPLETED, NODE_BUTTON_ONCOMPLETED_START_FRAME, NODE_BUTTON_ONCOMPLETED_END_FRAME, LOGO_FRAME_RATE, -1);
+	animator->play(ONOUT);
 }
 
 void MapScene::handleInput() {
