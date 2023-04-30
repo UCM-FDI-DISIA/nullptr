1e5d4596e83f89c25cfe6e2cee75eb2d9dd4cfb1
Alejandro González <90796690+algonz39@users.noreply.github.com>
2023-04-30 09:04:29 +0200

    Mas rango e invulnerabilidad

--- Game/Game/src/gameObjects/Card Objects/LaserKatana.cpp
+++ Game/Game/src/gameObjects/Card Objects/LaserKatana.cpp
@@ -5,12 +5,12 @@ void LaserKatanaCard::attack(Vector2D playerPos, Vector2D mousePos, float attack
 	Vector2D dir = (mousePos - playerPos - where->getCamera()->getOffset()).normalize();
 	float rotation = Vector2D(1, 0).angle(dir);
 
-	Hitbox::HitboxData data = { playerPos, VECTOR_ZERO, rotation, 220, 70, LASER, _grp_ENEMIES };
+	Hitbox::HitboxData data = { playerPos, VECTOR_ZERO, rotation, 250, 70, LASER, _grp_ENEMIES };
 	data.pos = data.pos + Vector2D(data.width / 2, 0);
 
 	where->addGameObject<Hitbox>(_grp_PLYR_ATTACK, damage * attackMult, false, false, DASH_DURATION, data, Vector2D(0, data.height / 2));
 	where->getPlayer()->getComponent<PlayerMovementComponent>()->dash(dir);
-	where->getPlayer()->getComponent<HealthComponent>()->setInvencibility(DASH_DURATION*1.75);
+	where->getPlayer()->getComponent<HealthComponent>()->setInvencibility(DASH_DURATION*3);
 }
 
 void LaserKatanaCard::ability(Vector2D playerPos, Vector2D mousePos, float attackMult, BattleScene* where) {
