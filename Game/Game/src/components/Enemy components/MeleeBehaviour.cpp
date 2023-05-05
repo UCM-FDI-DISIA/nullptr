b2a1c3877393427c51daff1f9ba1e8b0ea191cbb
josepk01 <72452701+josepk01@users.noreply.github.com>
2023-05-05 20:54:19 +0200

    Update MeleeBehaviour.cpp

--- Game/Game/src/components/Enemy components/MeleeBehaviour.cpp
+++ Game/Game/src/components/Enemy components/MeleeBehaviour.cpp
@@ -90,7 +90,7 @@ void MeleeBehaviour::enemyAttack() {
 		Vector2D attackPos = pos->getPos() + vel * 100;
 		float rotation = Vector2D(1, 0).angle(vel);
 		Hitbox::HitboxData data = { attackPos, VECTOR_ZERO, rotation, 200, 100, "NewSwordSpin", _grp_PLAYER };
-		gStt->addGameObject<Hitbox>(_grp_ENM_ATTACK, damage, true, 10, data, Vector2D(-1,-1),nullptr, gObj->getComponent<Transform>()->getPos());
+		gStt->addGameObject<Hitbox>(_grp_ENM_ATTACK, damage, true, 0.1, data, Vector2D(-1,-1),nullptr, gObj->getComponent<Transform>()->getPos());
 		//player->getComponent<HealthComponent>()->receiveDamage(0);
 	}
 }
